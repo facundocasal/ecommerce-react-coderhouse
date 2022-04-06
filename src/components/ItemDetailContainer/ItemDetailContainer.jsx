13e10b8a2ebcf186fa7b9ccc0c08ce8b4a2fd20d
Facundo Nahuel Casal <fcasal92@gmail.com>
2022-04-06 20:16:32 -0300

    agregado del spiner

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import ItemDetail from '../ItemDetail/ItemDetail'
 import getItem from './getItem'
+import { Spinner } from 'react-bootstrap';
 
 function ItemDetailContainer() {
 
@@ -11,7 +12,7 @@ function ItemDetailContainer() {
 
     //TERNARIO  number <10? setNumber (number + 1): alert("no se puede ")
     useEffect(()=>{
-        let time = 2000
+        let time = 3000
         let url = "https://api.mercadolibre.com/products/MLA10025564#json"
         setTimeout(() =>
         getItem(url)
@@ -24,10 +25,11 @@ function ItemDetailContainer() {
     },[])
 
   return (
-    <div>
-        {loading?(<h1> Cargando productos</h1>):(<ItemDetail item={item}/>)}
+    <>
         
-    </div>
+        {loading?(<div style={{display: "flex" , justifyContent:'space-around' , alignItems:"center"}}> <h1> Cargando Detalles Del Producto </h1> <Spinner animation="grow"/></div>):(<ItemDetail item={item}/>)}
+        
+    </>
   )
 }
 
